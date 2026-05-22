de23568e77ec615f840b1f8a9db40bc857fd6cd9
rosarioborgesi <borgesiros@gmail.com>
2026-05-22 14:51:35 +0200

    Improve addLiquidity tests and add fuzz testing for liquidity invariants

--- test/fuzz/UniswapV1ExchangeFuzzTest.t.sol
+++ test/fuzz/UniswapV1ExchangeFuzzTest.t.sol
@@ -9,6 +9,8 @@ contract UniswapV1ExchangeFuzzTest is Test {
     UniswapV1Exchange public exchange;
     ERC20Mock public token;
 
+    address user = makeAddr("user");
+
     function setUp() external {
         token = new ERC20Mock();
         exchange = new UniswapV1Exchange(address(token), "Uniswap V1", "UNI-V1");
@@ -70,5 +72,95 @@ contract UniswapV1ExchangeFuzzTest is Test {
         uint256 actualOutputPrice = exchange.getOutputPrice(_outputAmount, _inputReserve, _outputReserve);
 
         assertEq(expectedOutputPrice, actualOutputPrice);
+    }
+
+    //////////////////////////
+    //    addLiquidity    //
+    //////////////////////////
+    // Tests the invariant expectedLiquidity = (ethAmount * totalLiquidityBefore) / ethReserve;
+    function testAddLiquidityMintsCorrectLiquidity(uint256 _ethReserve, uint256 _tokenReserve, uint256 _ethAmount)
+        external
+    {
+        // Reserves range from 1 ether to type(uint112).max to be realistic
+        _ethReserve = bound(_ethReserve, 1 ether, type(uint112).max);
+        _tokenReserve = bound(_tokenReserve, 1 ether, type(uint112).max);
+        _ethAmount = bound(_ethAmount, 1 wei, type(uint112).max);
+
+        // First LP
+        deal(address(token), user, _tokenReserve);
+        deal(user, _ethReserve);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), _tokenReserve);
+
+        exchange.addLiquidity{value: _ethReserve}(0, _tokenReserve, block.timestamp + 1);
+
+        vm.stopPrank();
+
+        uint256 totalLiquidityBefore = exchange.totalSupply();
+
+        // Second LP
+        address lp2 = makeAddr("lp2");
+
+        uint256 requiredTokens = (_ethAmount * _tokenReserve) / _ethReserve + 1;
+
+        deal(address(token), lp2, requiredTokens);
+        deal(lp2, _ethAmount);
+
+        vm.startPrank(lp2);
+
+        token.approve(address(exchange), requiredTokens);
+
+        uint256 liquidityMinted =
+            exchange.addLiquidity{value: _ethAmount}(_ethAmount, requiredTokens, block.timestamp + 1);
+
+        vm.stopPrank();
+
+        uint256 expectedLiquidity = (_ethAmount * totalLiquidityBefore) / _ethReserve;
+
+        assertEq(liquidityMinted, expectedLiquidity);
+
+        uint256 totalLiquidityAfter = totalLiquidityBefore + liquidityMinted;
+        uint256 totalLiquidityAfterExpected = _ethReserve + expectedLiquidity;
+        assertEq(totalLiquidityAfter, totalLiquidityAfterExpected);
+    }
+
+    // Tests that addLiquidity preserves the ETH/token reserve ratio
+    function testAddLiquidityPreservesReserveRatio(uint256 _ethReserve, uint256 _tokenReserve, uint256 _ethAmount)
+        external
+    {
+        // Reserves range from 1 ether to type(uint112).max to be realistic
+        _ethReserve = bound(_ethReserve, 1 ether, type(uint112).max);
+        _tokenReserve = bound(_tokenReserve, 1 ether, type(uint112).max);
+        _ethAmount = bound(_ethAmount, 1 wei, type(uint112).max);
+
+        // Setup initial liquidity
+        deal(address(token), user, _tokenReserve);
+        deal(user, _ethReserve);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), _tokenReserve);
+
+        exchange.addLiquidity{value: _ethReserve}(0, _tokenReserve, block.timestamp + 1);
+        vm.stopPrank();
+
+        uint256 tokenAmount = (uint256(_ethAmount) * _tokenReserve) / _ethReserve + 1;
+
+        address lp2 = makeAddr("lp2");
+
+        deal(address(token), lp2, tokenAmount);
+        deal(lp2, _ethAmount);
+
+        vm.startPrank(lp2);
+        token.approve(address(exchange), tokenAmount);
+
+        exchange.addLiquidity{value: _ethAmount}(1, tokenAmount, block.timestamp + 1);
+
+        vm.stopPrank();
+
+        uint256 newEthReserve = address(exchange).balance;
+        uint256 newTokenReserve = token.balanceOf(address(exchange));
+
+        assertApproxEqAbs((newEthReserve * 1e18) / newTokenReserve, (_ethReserve * 1e18) / _tokenReserve, 0.006 ether);
     }
 }
--- test/unit/UniswapV1ExchangeUnitTest.t.sol
+++ test/unit/UniswapV1ExchangeUnitTest.t.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {UniswapV1Exchange} from "src/UniswapV1Exchange.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {IERC20Errors} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UniswapV1ExchangeUnitTest is Test {
     UniswapV1Exchange public exchange;
@@ -282,5 +283,176 @@ contract UniswapV1ExchangeUnitTest is Test {
 
         vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__InvalidRecipient.selector);
         exchange.ethToTokenTransferOutput{value: 1 ether}(100 ether, block.timestamp, address(0));
+    }
+
+    /////////////////////////
+    //    addLiquidity     //
+    /////////////////////////
+    modifier addLiquidity(uint256 _ethAmount, uint256 _tokenAmount) {
+        deal(address(token), user, _tokenAmount);
+        deal(user, _ethAmount);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), _tokenAmount);
+
+        exchange.addLiquidity{value: _ethAmount}(0, _tokenAmount, block.timestamp + 1);
+        vm.stopPrank();
+        _;
+    }
+
+    function testAddLiquidityFirstProviderMintsInitialLiquidity() external {
+        uint256 ethAmount = 10 ether;
+        uint256 tokenAmount = 20_000 ether;
+
+        deal(address(token), user, tokenAmount);
+        deal(user, ethAmount);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), tokenAmount);
+
+        uint256 liquidityMinted = exchange.addLiquidity{value: ethAmount}(0, tokenAmount, block.timestamp + 1);
+        vm.stopPrank();
+
+        assertEq(liquidityMinted, ethAmount);
+        assertEq(exchange.totalSupply(), ethAmount);
+        assertEq(exchange.balanceOf(user), ethAmount);
+        assertEq(address(exchange).balance, ethAmount);
+        assertEq(token.balanceOf(address(exchange)), tokenAmount);
+    }
+
+    function testAddLiquiditySecondProviderMintsProportionalLiquidity() external addLiquidity(10 ether, 20_000 ether) {
+        // First LP
+        uint256 ethAmountLp1 = 10 ether;
+        //uint256 tokenAmountLp1 = 20_000 ether;
+
+        // Second LP
+        uint256 ethAmountLp2 = 1 ether;
+        uint256 tokenAmountLp2 = 5_000 ether;
+        deal(alice, ethAmountLp2);
+        deal(address(token), alice, tokenAmountLp2);
+
+        vm.startPrank(alice);
+        token.approve(address(exchange), tokenAmountLp2);
+        uint256 liquidityMinted =
+            exchange.addLiquidity{value: ethAmountLp2}(ethAmountLp2, tokenAmountLp2, block.timestamp + 1);
+        vm.stopPrank();
+
+        assertEq(liquidityMinted, ethAmountLp2);
+        assertEq(exchange.totalSupply(), ethAmountLp1 + ethAmountLp2);
+        assertEq(exchange.balanceOf(alice), ethAmountLp2);
+
+        // Token amount LP 2 = tokenAmount = msg.value * tokenReserve / ethReserve + 1 = (1 * 20_000 / 10) + 1
+        // Total token amount = Token Amount LP 1 + token amount LP 2 = 2_000 + 1 + 20_000 = 22_000 + 1
+        assertEq(token.balanceOf(address(exchange)), 22_000 ether + 1);
+    }
+
+    function testAddLiquidityRevertsIfDeadlineExpired() external {
+        uint256 ethAmount = 10 ether;
+        uint256 tokenAmount = 20_000 ether;
+
+        deal(address(token), user, tokenAmount);
+        deal(user, ethAmount);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), tokenAmount);
+
+        vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__DeadlineExpired.selector);
+        exchange.addLiquidity{value: ethAmount}(ethAmount, tokenAmount, block.timestamp);
+        vm.stopPrank();
+    }
+
+    function testAddLiquidityRevertsIfMaxTokensIsZero() external {
+        uint256 ethAmount = 10 ether;
+        uint256 tokenAmount = 20_000 ether;
+
+        deal(address(token), user, tokenAmount);
+        deal(user, ethAmount);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), tokenAmount);
+
+        vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__MaxTokensIsZero.selector);
+        exchange.addLiquidity{value: ethAmount}(ethAmount, 0, block.timestamp + 1);
+        vm.stopPrank();
+    }
+
+    function testAddLiquidityRevertsIfEthAmountIsZero() external {
+        uint256 ethAmount = 10 ether;
+        uint256 tokenAmount = 20_000 ether;
+
+        deal(address(token), user, tokenAmount);
+        deal(user, ethAmount);
+
+        vm.startPrank(user);
+        token.approve(address(exchange), tokenAmount);
+
+        vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__InsufficientEthAmount.selector);
+        exchange.addLiquidity(ethAmount, tokenAmount, block.timestamp + 1);
+        vm.stopPrank();
+    }
+
+    function testAddLiquidityRevertsIfMinLiquidityIsZeroAfterPoolExists()
+        external
+        addLiquidity(10 ether, 20_000 ether)
+    {
+        // First LP
+        uint256 ethAmountLp1 = 10 ether;
+        //uint256 tokenAmountLp1 = 20_000 ether;
+
+        // Second LP
+        uint256 ethAmountLp2 = 1 ether;
+        uint256 tokenAmountLp2 = 5_000 ether;
+        deal(alice, ethAmountLp2);
+        deal(address(token), alice, tokenAmountLp2);
+
+        vm.startPrank(alice);
+        token.approve(address(exchange), tokenAmountLp2);
+
+        vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__MinLiquidityIsZero.selector);
+        exchange.addLiquidity{value: ethAmountLp2}(0, tokenAmountLp2, block.timestamp + 1);
+        vm.stopPrank();
+    }
+
+    function testAddLiquidityRevertsIfMaxTokensExceeded() external addLiquidity(10 ether, 20_000 ether) {
+        // First LP
+        //uint256 ethAmountLp1 = 10 ether;
+        //uint256 tokenAmountLp1 = 20_000 ether;
+
+        // Second LP
+        uint256 ethAmountLp2 = 1 ether;
+        uint256 tokenAmountLp2 = 2_000 ether;
+        deal(alice, ethAmountLp2);
+        deal(address(token), alice, tokenAmountLp2);
+
+        vm.startPrank(alice);
+        token.approve(address(exchange), tokenAmountLp2);
+
+        vm.expectRevert(UniswapV1Exchange.UniswapV1Exchange__MaxTokensExceeded.selector);
+        // _maxTokens = tokenAmountLp2 = 2_000
+        // tokenAmount = msg.value * tokenReserve / ethReserve + 1 = 1 * 20_000 / 10 + 1 = 2_001
+        // _maxTokens < tokenAmount because 2_000 < 2_001
+        exchange.addLiquidity{value: ethAmountLp2}(tokenAmountLp2, tokenAmountLp2, block.timestamp + 1);
+        vm.stopPrank();
+    }
+
+    function testLpTokensCanBeTransferred() external addLiquidity(10 ether, 20_000 ether) {
+        vm.prank(user);
+        exchange.transfer(alice, 1 ether);
+
+        assertEq(exchange.balanceOf(user), 9 ether);
+        assertEq(exchange.balanceOf(alice), 1 ether);
+    }
+
+    function testLpTokenTransferRevertsIfAmountExceedsBalance() external addLiquidity(10 ether, 20_000 ether) {
+        uint256 userLiquidity = exchange.balanceOf(user);
+
+        vm.startPrank(user);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientBalance.selector, user, userLiquidity, userLiquidity + 1
+            )
+        );
+        exchange.transfer(alice, userLiquidity + 1);
+        vm.stopPrank();
     }
 }
